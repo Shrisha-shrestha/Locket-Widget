daff8d9919fe2bcc78ea2ca194b60aa3245b6587
shrisha <shrisha.shrestha@uranustechnepal.com>
2024-09-03 16:29:11 +0545

    feat: :sparkles: implement homescreen widgets following the codelab tutorial and adapting files

--- android/app/src/main/java/com/example/locket_widget/LocketWidget.kt
+++ android/app/src/main/java/com/example/locket_widget/LocketWidget.kt
@@ -5,40 +5,28 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 
-/**
- * Implementation of App Widget functionality.
- */
+import com.example.locket_widget.R
+import es.antonborri.home_widget.HomeWidgetPlugin
+
 class LocketWidget : AppWidgetProvider() {
     override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetIds: IntArray,
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            // Get reference to SharedPreferences
+            val widgetData = HomeWidgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName, R.layout.locket_widget).apply {
+
+                val title = widgetData.getString("headline_title", null)
+                setTextViewText(R.id.headline_title, title ?: "No title set")
+
+                val description = widgetData.getString("headline_description", null)
+                setTextViewText(R.id.headline_description, description ?: "No description set")
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
-
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.locket_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
