206c8b7fa8d425b22ba4ee39869df0dbe18dc334
Shrisha Shrestha <51104421+Shrisha-shrestha@users.noreply.github.com>
2024-09-04 23:33:22 +0545

    chore: :wrench: configure firebase for project
    Registered android, ios, web and windows app on firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:locket_widget/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
